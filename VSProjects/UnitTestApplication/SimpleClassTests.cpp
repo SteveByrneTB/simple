0346143601ecd60a8f1f00abe79b5548aa4b4f06
Steven Byrne <steve.byrne@touchbionics.com>
2018-05-08 10:07:01 +0100

    Add doxygen support and CCS project

--- VSProjects/UnitTestApplication/SimpleClassTests.cpp
+++ VSProjects/UnitTestApplication/SimpleClassTests.cpp
@@ -15,3 +15,8 @@ TEST(SimpleClassTests, ComparisonAEqualB) {
 	SimpleClass sc;
 	EXPECT_TRUE(sc.Comparison(2, 2));
 }
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
